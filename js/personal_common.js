5c30258146f57e23638cb8c5702576c89e64487a
18224429446 <904478962@qq.com>
2016-12-06 23:27:42 +0800

    bootstrap HTML
    Signed-off-by: 18224429446 <904478962@qq.com>

---
+++ js/personal_common.js
@@ -0,0 +1,37 @@
+/**
+ * Created by admin on 2016/12/4.
+ */
+var txt='<ul id="menu" class="list-group">\
+    <li class="list-group-item">\
+        <a href="#" class="menu-title menu-title-border"><span>投资项目</span></a>\
+    </li>\
+    <li class="list-group-item">\
+        <a href="#" class="menu-title"><span>借款项目</span></a>\
+        <ul>\
+            <li><a href="borrowing_produce.html">借款项目</a></li>\
+            <li><a href="#">还款明细</a></li>\
+        </ul>\
+    </li>\
+    <li class="list-group-item">\
+        <a href="#" class="menu-title"><span>我的账户</span></a>\
+        <ul>\
+            <li class="active"><a href="#">账户信息</a></li>\
+            <li><a href="real_name.html">实名认证</a></li>\
+            <li><a href="#">银行卡管理</a></li>\
+            <li><a href="#">登录记录</a></li>\
+        </ul>\
+    </li>\
+    <li class="list-group-item">\
+        <a href="#" class="menu-title"><span>资产详情</span></a>\
+        <ul>\
+            <li><a href="#">账户流水</a></li>\
+            <li><a href="recharge.html">充值明细</a></li>\
+            <li><a href="#">提现记录</a></li>\
+            <li><a href="#">收款明细</a></li>\
+        </ul>\
+    </li>\
+    <li class="list-group-item">\
+        <a href="userinfo.html" class="menu-title menu-title-border"><span>个人资料</span></a>\
+    </li>\
+    </ul>';
+document.write(txt);
